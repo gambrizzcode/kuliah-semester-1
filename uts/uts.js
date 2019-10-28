c0009e166eca1c06b418353b0de6e32c642ead3f
gambrizzcode <permanen29@gmail.com>
2019-10-28 23:32:28 +0700

    tugas uts fix

--- uts/uts.js
+++ uts/uts.js
@@ -3,31 +3,38 @@
 function keluar() {
 	var jml = document.getElementById("jml").value;
 
+	document.write("<h2 align='center'>Pengisian Nilai Mahasiswa </h2><hr>");
+
 	for (var i = 1; i <= jml; i++) {
-		document.write("<h3>Mahasiswa "+i+"</h3><br>");
+		document.write("<center><div style='margin: 10px; padding: 10px; width: 500px;'>");
+		document.write("<h3>Mahasiswa "+i+"</h3>");
 		document.write("<table>");
 		document.write("<tr><td>NIM</td>");
-		document.write("<td><input type='text' id='nim"+i+"'></td></tr>");
+		document.write("<td> : <input type='text' style='width:300px' id='nim"+i+"'></td></tr>");
 		document.write("<tr><td>Nama Lengkap</td>");
-		document.write("<td><input type='text' id='nama"+i+"'></td></tr>");
+		document.write("<td> : <input type='text' style='width:300px' id='nama"+i+"'></td></tr>");
 		document.write("<tr><td>Nilai Tugas 1</td>");
-		document.write("<td><input type='text' id='tugas1"+i+"'></td></tr>");
+		document.write("<td> : <input type='number' style='width:300px' id='tugas1"+i+"'></td></tr>");
 		document.write("<tr><td>Nilai Tugas 2</td>");
-		document.write("<td><input type='text' id='tugas2"+i+"'></td></tr>");
+		document.write("<td> : <input type='number' style='width:300px' id='tugas2"+i+"'></td></tr>");
 		document.write("<tr><td>Nilai UTS</td>");
-		document.write("<td><input type='text' id='uts"+i+"'></td></tr>");
+		document.write("<td> : <input type='number' style='width:300px' id='uts"+i+"'></td></tr>");
 		document.write("<tr><td>Nilai UAS</td>");
-		document.write("<td><input type='text' id='uas"+i+"'><br></td></tr>");
+		document.write("<td> : <input type='number' style='width:300px' id='uas"+i+"'><br></td></tr>");
 		document.write("</table>");
+		document.write("</div></center>");
 	}
 
-	document.write("<hr><input type='submit' name='submit' value='SUBMIT' onclick='uhuy("+jml+")'><br>");
+	document.write("<hr><center><div style='width:450px; text-align: center'>");
+	document.write("<input type='submit' style='width: 250px; height: 35px; font-size: 20px; margin-left:25px; margin-top: 10px' name='submit' value='SUBMIT' onclick='uhuy("+jml+")'>");
+	document.write("</div></center><br>");
 }
 
 function uhuy(yuhu) {
-	document.write("<br>");
-	document.write("<table border='1' style='border-collapse:collapse'>");
-	document.write("<tr><th>No</th>");
+	document.write("<br><center>");
+	document.write("<div style='width:70%;'>");
+	document.write("<table width='100%' border='1' style='border-collapse:collapse'>");
+	document.write("<tr height='30px'><th>No</th>");
 	document.write("<th>NIM</th>");
 	document.write("<th>Nama Lengkap</th>");
 	document.write("<th>Nilai Tugas 1</th>");
@@ -65,7 +72,7 @@ function uhuy(yuhu) {
 			huruf = "E"; status = "TIDAK LULUS";
 		}
 
-		document.write("<tr><td>"+j+"</td>");
+		document.write("<tr height='30px' style='padding:5px; margin:5px'><td>"+j+"</td>");
 		document.write("<td>"+aha[0]+"</td>");
 		document.write("<td>"+aha[1]+"</td>");
 		document.write("<td>"+aha[2]+"</td>");
@@ -76,5 +83,5 @@ function uhuy(yuhu) {
 		document.write("<td>"+huruf+"</td>");
 		document.write("<td>"+status+"</td></tr>");
 	}
-	document.write("</table>");
+	document.write("</table></center><br><hr>");
 }
