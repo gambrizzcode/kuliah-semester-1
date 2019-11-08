31dde26a27011d2004e21643413dbf61f0b67a21
gambrizzcode <permanen29@gmail.com>
2019-11-08 15:54:48 +0700

    pemdas semester 1

---
+++ coba/anu.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// var a = 10;
+var a = prompt("masukkan jumlah");
+var b = 1;
+var c = prompt("masukkan nama");
+
+while(b <= a){
+	document.write(c + "<br>");
+	b++;
+}
+
+// do{
+	// document.write(c + "<br>");
+	// b++;
+// }while(b <= a);
