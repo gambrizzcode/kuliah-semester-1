0d6953f23b5bb67526e756d334ee0ed799400c85
gambrizzcode <permanen29@gmail.com>
2019-11-08 13:17:45 +0700

    pemdas semester 1

--- sort.php
+++ sort.php
@@ -1,20 +1,47 @@
 <?php
 //bubble sort
 
-$a = array(4,3,1,2,6,5);
+$a = array(1,2,2,2,5);
 $n = count($a);
+$jml = 1;
 
-// for ($i=0; $i < $n-1; $i++) { 
-// 	// echo $i;
-// 	for ($j=$i+1; $j < $n ; $j++) { 
-// 		// echo $j;
-// 		if ($a[$i] > $a[$j]) {
-// 			$temp    = $a[$i];
-// 			$a[$i] = $a[$j];
-// 			$a[$j]   = $temp; 
-// 		}
-// 	}
-// }
+for ($i=0; $i < $n-1; $i++) { 
+	// echo $i,",";
+	for ($j=$i+1; $j < $n ; $j++) { 
+		// echo $j.",";
+		if ($a[$i] > $a[$j]) {
+			$temp    = $a[$i];
+			$a[$i] 	 = $a[$j];
+			$a[$j]   = $temp; 
+		}
+	}
+}
+
+// print_r($a);
+// $w[] = null;
+// $jml[] = null;
+// $jml[] = null;
+for ($ii=0; $ii < $n-1; $ii++) { 
+	// echo $ii,",";
+	for ($jj=$ii+1; $jj < $n ; $jj++) { 
+		// echo $a[$jj].",";
+		if ($a[$ii] == $a[$jj]) {
+			// echo $a[$ii].",";
+			// $jml[$ii] += 1;
+			// $jml[$j] = $ju++;
+			// $a[$i] 	 = $a[$j];
+			// $a[$j]   = $temp; 
+		}elseif($a[$ii] != $a[$jj]){
+			// echo $a[$ii].",";
+		}
+	}
+}
+
+print_r($a);
+
+foreach ($a as $bil) {
+	// echo $bil.":".$jml."<br>";
+}
 
 // print_r($a);
 
