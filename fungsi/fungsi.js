35ecc299a03e5d9def324ea472041a4d8f59bc52
gambrizzcode <permanen29@gmail.com>
2019-11-15 17:10:32 +0700

    latihan function

---
+++ fungsi/fungsi.js
@@ -0,0 +1,43 @@
+// var halocoba = function cobahalo() {
+// 	document.write('hallo semua', 'selamat malam');
+// }
+
+// halocoba();
+
+function jumlahab(a,b) {
+	return a+b;
+}
+
+var halo = function(x) {
+	for (var i = 0; i < x; i++) {
+		document.write("kucing <br>");
+	}
+}
+
+// var hasil = jumlahab(2,8);
+// document.write("hasil dari function jumlahab a + b = " + hasil + "<br>");
+
+// document.write("hasil dari function jumlahab a + b = " + jumlahab(4,5) + "<br>");
+
+// halo(2);
+
+function fungsiku(){
+	alert('pemanggilan dengan window.namafungsi()');
+}
+
+function tombolklik(){
+	alert('klik bangsat !');
+}
+
+function ana(x){
+	function budi(y){
+		function cici(z){
+			document.write("fungsi ana, budi, cici adalah = " + x + y + z + "<br>");
+			var hasil = x+y+z;
+			document.write("hasil penjumlahan keseluruhan adalah = " +  hasil + "<br>");
+		}
+		cici(2);
+	}
+	budi(8);
+}
+ana(4);
